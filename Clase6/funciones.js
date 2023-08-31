b54b731d3d72cfbe91c29a29ba604abf550d8baf
FAMellado <128266640+FAMellado@users.noreply.github.com>
2023-08-31 11:54:51 -0400

    Clase 6

---
+++ Clase6/funciones.js
@@ -0,0 +1,37 @@
+console.log("Hola que tal");
+
+/*
+$(document).ready( function(){
+    alert("Hellow que sucede realmente!!");
+});
+*/
+
+function hola() {
+    alert("SOCIAL CREDIT SCORE -99999999999999999");
+}
+
+
+
+
+function cambiarAzul(){
+
+    document.getElementById("poroto").style.backgroundColor = "blue";
+
+}
+
+
+
+function cambiarVerde(){
+
+    document.getElementsByClassName("almejas")[0].style.backgroundColor = "green";
+
+}
+
+
+
+function cambiarOriginal(){
+
+    document.getElementById("poroto").style.backgroundColor = "red";
+
+    //NO SE PUEDE POR getElementBy NAME 
+}
